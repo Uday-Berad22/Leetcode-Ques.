fd29a966d54f5c6855e7b770c57899c077bcc639
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-16 15:10:13 +0530

    Time: 202 ms (27.73%), Space: 25.1 MB (30.58%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,28 +1,32 @@
 class Solution {
     vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size(); 
-        priority_queue <pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        vector<vector<int>> val(n,vector<int> (m,INT_MAX));
-        val[0][0]=0;
-        pq.push({0,{0,0}});
-        while(!pq.empty()){
-            int i=pq.top().second.first;
-            int j=pq.top().second.second;
-            int value=pq.top().first;
-            pq.pop();
-            if(i==n-1&&j==m-1) {return value;}
-            for(auto dir: directions){
-                if(i+dir[0]>=0&&j+dir[1]>=0&&i+dir[0]<n&&j+dir[1]<m){
-                    if(val[i+dir[0]][j+dir[1]]>(abs(heights[i][j]-heights[i+dir[0]][j+dir[1]]),value)){
-                        val[i+dir[0]][j+dir[1]]=max(abs(heights[i][j]-heights[i+dir[0]][j+dir[1]]),value);
-                        pq.push({val[i+dir[0]][j+dir[1]],{i+dir[0],j+dir[1]}});
+    bool check_DFS(vector<vector<int>> &heights,int &threeShold,int i,int j,vector<vector<bool>> &visited){
+         int n=heights.size();int m=heights[0].size();
+         if(i==n-1&&j==m-1) return true;
+         visited[i][j]=true;
+         for(auto &dir: directions){
+             if((i+dir[0]>=0&&j+dir[1]>=0)&&(i+dir[0]<n&&j+dir[1]<m)&&(visited[i+dir[0]][j+dir[1]]==false&&abs(heights[i][j]-heights[i+dir[0]][j+dir[1]])<=threeShold)){
+                    if(check_DFS(heights,threeShold,i+dir[0],j+dir[1],visited)==true){
+                        return true;
                     }
                 }
+         }
+         return false;
+    }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int low=-1;
+        int high=1e6;
+        while(high-low>1){
+            int mid=(high+low)/2;
+            vector<vector<bool>> visited(heights.size(),vector<bool> (heights[0].size(),false));
+            if(check_DFS(heights,mid,0,0,visited)==true){
+                high=mid;
             }
-        } 
-        return INT_MAX;
+            else{
+                low=mid;
+            }
+        }
+        return high;
     }
 };
