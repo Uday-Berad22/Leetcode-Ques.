9de637cdd8067857f5c04b9c7b9181d83f9d603f
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-16 14:38:28 +0530

    Time: 276 ms (19.72%), Space: 52.6 MB (10.97%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,45 +1,28 @@
 class Solution {
     vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-    bool check(vector<vector<int>> &heights,int &threeShold){
-        priority_queue<pair<int,int>> q;
+    int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
-        int m=heights[0].size();
-        vector<vector<bool>> visited(n,vector<bool> (m,false));
-        q.push({0,0});
-        visited[0][0]=true;
-        int count=0;
-        while(!q.empty()){
-            int i=q.top().first;
-            int j=q.top().second;
-            q.pop();
-
-            count++;
-            // if(count>100) return false;
-            visited[i][j]=true;
-            if(i==n-1&&j==m-1) return true;
+        int m=heights[0].size(); 
+        priority_queue <pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> val(n,vector<int> (m,INT_MAX));
+        val[0][0]=0;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            int i=pq.top().second.first;
+            int j=pq.top().second.second;
+            int value=pq.top().first;
+            pq.pop();
+            if(i==n-1&&j==m-1) {return value;}
             for(auto dir: directions){
-                if((i+dir[0]>=0&&j+dir[1]>=0)&&(i+dir[0]<n&&j+dir[1]<m)&&(visited[i+dir[0]][j+dir[1]]==false&&abs(heights[i][j]-heights[i+dir[0]][j+dir[1]])<=threeShold)){
-                    q.push({i+dir[0],j+dir[1]});
-                    visited[i+dir[0]][j+dir[1]]=true;
+                if(i+dir[0]>=0&&j+dir[1]>=0&&i+dir[0]<n&&j+dir[1]<m){
+                    if(val[i+dir[0]][j+dir[1]]>(abs(heights[i][j]-heights[i+dir[0]][j+dir[1]]),value)){
+                        val[i+dir[0]][j+dir[1]]=max(abs(heights[i][j]-heights[i+dir[0]][j+dir[1]]),value);
+                        pq.push({val[i+dir[0]][j+dir[1]],{i+dir[0],j+dir[1]}});
+                    }
                 }
             }
-        }
-        return false;
-    }
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int low=-1;
-        int high=1e7;
-        int count=0;
-        while(high-low>1){
-            int mid=(high+low)/2;
-            if(check(heights,mid)==true){
-                high=mid;
-            }
-            else{
-                low=mid;
-            }
-        }
-        return high;
+        } 
+        return INT_MAX;
     }
 };
