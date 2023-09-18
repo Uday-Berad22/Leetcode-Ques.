443b6e686d2c0fa47cce050c0531a5f1b8a286aa
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-18 19:47:39 +0530

    Time: 150 ms (50.98%), Space: 67.3 MB (28.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -7,18 +7,28 @@ public:
             s.insert(a);
         }
         int ans=0;
+        auto a=s.begin();
+        int cnt=0;
         while(s.size()>0){
-            auto a=*(s.begin());
-            m[a]=1;
-            int i=1;
-            s.erase(a);
-            while(s.find(a+i)!=s.end()){
-                m[a]++;
-                s.erase(a+i);
-                i++;
+            int x=(*(a))-1;
+            // cout<<x+1<<" "<<cnt<<" "<<ans<<endl;
+            if(s.find(x)!=s.end()){
+                a++;
+                cnt=0;
             }
-            m[a]+=m[a+i];
-             ans=max(ans,m[a]);
+            else{
+                s.erase(x+1);
+                cnt++;
+                ans=max(cnt,ans);
+                if(s.find((x)+2)!=s.end()){
+                    a=s.find((x)+2);
+                }
+                else{
+                     a=s.begin();
+                     cnt=0;
+                }
+            }
+            
         }
         return ans;
     }
