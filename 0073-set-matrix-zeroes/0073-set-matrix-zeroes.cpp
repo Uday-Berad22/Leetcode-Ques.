6418c12e201b8464e13c2f5b9a3a5620b7db1842
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-18 21:12:10 +0530

    Time: 8 ms (88.51%), Space: 13.8 MB (9.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int flag=0;
+
+        //Checking if any zero is present in first row if present then flag=1;
         for(int col=0;col<matrix[0].size();col++){
             if(matrix[0][col]==0){ flag=1;break;}
         }
+
+        // if Zero is present in a row then make row's first element as zero and
+        // make first row having that column  element as zero
+        // e.g if a[1][2]==0 then make a[1][0]=0 and a[0][2]=0
         for(int row=1;row<matrix.size();row++){
             for(int col=0;col<matrix[0].size();col++){
                 if(matrix[row][col]==0){
@@ -13,6 +19,9 @@ public:
                 }
             }
         }
+
+
+         //Making rows as zeroes if zero is present in it
         for(int row=1;row<matrix.size();row++){
             if(matrix[row][0]==0){
                 for(int col=0;col<matrix[0].size();col++){
@@ -20,6 +29,8 @@ public:
                 }
             }
         }
+
+       //Making coloumn as zeroes if zero is present in it
         for(int col=0;col<matrix[0].size();col++){
             if(matrix[0][col]==0){
                 for(int row=1;row<matrix.size();row++){
@@ -27,7 +38,9 @@ public:
                 }
             }
         }
-        if(flag==1){
+
+        // Checking if any zeroes is present in first row then make first row elements as zeroes
+        if(flag==1){ 
             for(int col=0;col<matrix[0].size();col++){
                  matrix[0][col]=0;
             }
