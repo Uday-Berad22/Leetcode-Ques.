142a9d4f5d273ba970083b817cd4a8acd4d773ac
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-22 12:59:16 +0530

    Time: 3 ms (41.31%), Space: 9.8 MB (6.91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
+    vector<vector<int>> Pascals_Triangle;
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<int> cur,prev;
-        vector<vector<int>> Pascals_Triangle;
-        for(int i=0;i<numRows;i++){
-            cur.resize(i+1,1);
-            for(int j=1;j<i;j++){
-                cur[j]=prev[j]+prev[j-1];
-            }
-            prev=cur;
-            Pascals_Triangle.push_back(cur);
+        vector<int> cur_row;
+        if(numRows==1){
+            cur_row.push_back(1);
+            Pascals_Triangle.push_back(cur_row);
+            return Pascals_Triangle;
         }
+        cur_row.push_back(1);
+        vector<int> prev=generate(numRows-1).back();
+        for(int i=1;i<numRows-1;i++){
+            cur_row.push_back(prev[i]+prev[i-1]);
+        }
+        cur_row.push_back(1);
+        Pascals_Triangle.push_back(cur_row);
         return Pascals_Triangle;
     }
 };
