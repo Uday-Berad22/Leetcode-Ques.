06b2d0673d462f1777a47450e4141f250b38a432
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-26 19:49:57 +0530

    Time: 11 ms (30.95%), Space: 14.4 MB (59.07%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.cpp
+++ 0815-champagne-tower/0815-champagne-tower.cpp
@@ -10,7 +10,14 @@ public:
         return dp[i][j]=a+b;
     }
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(query_row+1,vector<double> (query_row+1,-1));
-        return min(ans(query_row,query_glass,poured,dp),1.0);
+        vector<vector<double>> dp(query_row+1,vector<double> (query_row+1,0.0));
+        dp[0][0]=poured;
+        for(int i=0;i<query_row;i++){
+            for(int j=0;j<=i;j++){
+                dp[i+1][j]+=max(0.0,(dp[i][j]-1)/2);
+                dp[i+1][j+1]+=max(0.0,(dp[i][j]-1)/2);
+            }
+        }
+        return min(1.0,dp[query_row][query_glass]);
     }
 };
