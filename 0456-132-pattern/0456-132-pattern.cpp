aa16593d5a59212555c73dbb39660251a302ae7b
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-30 10:53:29 +0530

    Time: 60 ms (88.25%), Space: 48.9 MB (37.36%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> front(n);
-        front[0]=nums[0];
-        for(int i=1;i<n;i++){
-            front[i]=min(nums[i-1],front[i-1]);
-        }
-        set<int> st;
-        st.insert(nums[n-1]);
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]>front[i]){
-                auto it=st.upper_bound(front[i]);
-                if(it!=st.end()){
-                    if((*it)>front[i]&&(*it)<nums[i]){
-                        return true;
-                    }
-                }
-                st.insert(nums[i]);
+        stack<int> s;
+        int third = INT_MIN;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+            while (!s.empty() && s.top() < nums[i]) {
+                third = s.top();
+                s.pop();
             }
+            s.push(nums[i]);
         }
         return false;
-        
     }
 };
