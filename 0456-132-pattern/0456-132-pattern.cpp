54bcaa4afb798ddab3e059e32975e75a30cfc505
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-30 10:45:47 +0530

    Time: 111 ms (8.98%), Space: 57.2 MB (7.53%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -7,17 +7,17 @@ public:
         for(int i=1;i<n;i++){
             front[i]=min(nums[i-1],front[i-1]);
         }
-        priority_queue <int, vector<int>, greater<int> > pq;
-        pq.push(nums[n-1]);
+        set<int> st;
+        st.insert(nums[n-1]);
         for(int i=n-2;i>=0;i--){
             if(nums[i]>front[i]){
-                while(!pq.empty()&&pq.top()<=front[i]){
-                    pq.pop();
+                auto it=st.upper_bound(front[i]);
+                if(it!=st.end()){
+                    if((*it)>front[i]&&(*it)<nums[i]){
+                        return true;
+                    }
                 }
-                if(!pq.empty()&&pq.top()<nums[i]&&pq.top()>front[i]){
-                    return true;
-                }
-                pq.push(nums[i]);
+                st.insert(nums[i]);
             }
         }
         return false;
