1cd99f834d1e274598652bd3e09455e788b1871c
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-01 12:39:54 +0530

    Time: 610 ms (59.27%), Space: 217 MB (62.49%) - LeetHub

---
+++ 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.cpp
@@ -0,0 +1,83 @@
+class Node{
+    public: 
+    Node* link[2];
+    Node(){
+        link[0]=NULL;
+        link[1]=NULL;
+    }
+};
+class Trie{
+    public: 
+    Node *root;
+    Trie(){
+        root=new Node();
+    }
+    void insert(int num){
+        Node *node=root;
+        for(int i=31;i>=0;i--){
+            int x=((num>>i)&1);
+            if(node->link[x]==NULL){
+                node->link[x]=new Node();
+            }
+            node=node->link[x];
+        }
+    }
+    int max_xor(int x){
+        int ans=0;
+        Node *node=root;
+        for(int i=31;i>=0;i--){
+               int bit=((x>>i)&1); 
+               if(node!=NULL){
+               if(node->link[1-bit]!=NULL){
+                ans=ans|(1<<i);
+                node=node->link[1-bit];
+                }
+                else
+                node=node->link[bit];
+         }else return -1;
+               }
+        //  if(ans==0)return -1;
+        return ans;
+    }
+};
+class oQc{
+    public:
+    int val;
+    int x; 
+    int ind;
+    oQc(int val,int x,int ind){
+        this->val=val;
+        this->x=x;
+        this->ind=ind;
+    }
+};
+bool cmp(oQc &obj1,oQc&obj2){
+    return obj1.val<obj2.val;
+}
+#define arr nums
+class Solution {
+public:
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int q=queries.size();
+        int n=nums.size();
+        vector<oQc> oQ;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        for(auto &query: queries){
+            oQc obj(query[1],query[0],i);
+            i++;
+            oQ.push_back(obj);
+        }
+        sort(oQ.begin(),oQ.end(),cmp);
+        Trie T;
+        int index=0;
+        vector<int> fans(q,-1);
+        for(auto a: oQ){
+            while(index<n&&arr[index]<=a.val){
+                T.insert(arr[index]);index++;
+            }
+            fans[a.ind]=(T.max_xor(a.x));
+        }
+        return fans;
+    }
+};
