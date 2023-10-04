2a54c2e5158072c05214af36eec2e17ff1da6da6
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-04 14:43:39 +0530

    Time: 135 ms (24.05%), Space: 54 MB (62.2%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,24 +1,22 @@
-vector<int> v(1000000);
+vector<int> v(1000001);
 class MyHashMap {
-    unordered_map<int,int> m; 
 public:
     MyHashMap() {
-        for(int i=0;i<1000000;i++){
-            v[i]=0;
+        for(int i=0;i<=1000000;i++){
+            v[i]=-1;
         }
     }
     
     void put(int key, int value) {
-        m[key]= value;
+        v[key]= value;
     }
     
     int get(int key) {
-        return m.find(key)==m.end()?-1:m[key];
+        return v[key];
     }
     
     void remove(int key) {
-        if(m.find(key)!=m.end())
-        m.erase(key);
+        v[key]=-1;
     }
 };
 
