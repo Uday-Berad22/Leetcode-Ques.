63df5ee5018fd7e48fe446418583d5c299fe652a
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-12 11:22:01 +0530

    Time: 232 ms (82.17%), Space: 78.8 MB (98.35%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,17 +1,21 @@
+#define  flower flowers
 class Solution {
+    int M=1e5;
 public:
-    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : persons) {
-            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
-            res.push_back(started - ended);
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        //Current Flower= Number of flowers bloom - Number of flowers died 
+        vector<int> bloom(flowers.size()),endedbloom(flowers.size());
+        for(int i=0;i<flowers.size();i++){
+            bloom[i]=flowers[i][0];
+            endedbloom[i]=flowers[i][1];
         }
-        return res;
+        sort(bloom.begin(),bloom.end());
+        vector<int> ans(people.size(),0);
+        sort(endedbloom.begin(),endedbloom.end());
+        for(int i=0;i<people.size();i++){
+            ans[i]=upper_bound(bloom.begin(),bloom.end(),people[i])-bloom.begin();
+            ans[i]=ans[i]-(lower_bound(endedbloom.begin(),endedbloom.end(),people[i])-endedbloom.begin());
+        }
+        return ans;
     }
 };
