3aa255d7ae71da0a4e1eab5e5b781780867e5456
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-13 08:40:13 +0530

    Time: 6 ms (38.38%), Space: 14 MB (37.3%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int fun(int i,vector<int> &dp,vector<int> &cost){
-        int n=cost.size();
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int x=fun(i+1,dp,cost)+cost[i];
-        int y=fun(i+2,dp,cost)+cost[i];
-        return dp[i]=min(x,y);
-    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        int a=0,b=0;
+        int next=0,next_to_next=0;
         for(int i=n-1;i>=0;i--){
-            int c;
-            c=min(a,b)+cost[i];
-            b=a;
-            a=c;
+            int current;
+            current=min(next,next_to_next)+cost[i];
+            next_to_next=next;
+            next=current;
         }
-        return min(a,b);
+        return min(next,next_to_next);
     }
 };
