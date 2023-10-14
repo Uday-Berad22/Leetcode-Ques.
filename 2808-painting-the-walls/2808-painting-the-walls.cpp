dbb96ef159784d769ea1b94fb6b0df39597da34c
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-14 19:52:23 +0530

    Time: 1080 ms (5.15%), Space: 368.8 MB (6.4%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int fun(int i,int timetaken,int n,int taken,vector<int> &time,vector<vector<int>> &dp,vector<int> &cost){
-        if(i>=n ) { if(taken+timetaken>=n)return 0;return 9000000000;}
-        if(dp[i][timetaken+taken]!=-1) return dp[i][timetaken+taken];
-        if(timetaken>=n-taken){
-            return 0;
-        }
+        if(taken<=0) return 0;
+        if(i>=n ) { return 9000000000;}
+        if(dp[i][taken]!=-1) return dp[i][taken];
         int x=fun(i+1,timetaken,n,taken,time,dp,cost);
-        int y=fun(i+1,timetaken+time[i],n,taken+1,time,dp,cost)+cost[i];
-        return dp[i][timetaken+taken]=min(x,y);
+        int y=fun(i+1,timetaken+time[i],n,taken-1-time[i],time,dp,cost)+cost[i];
+        return dp[i][taken]=min(x,y);
     }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         vector<vector<int>> dp(501,vector<int> (1002,-1));
         int n=cost.size();
-        return fun(0,0,n,0,time,dp,cost);
+        return fun(0,0,n,n,time,dp,cost);
     }
 };
