bd0085f8e51a48ae84ae932a0b992cda709c7f80
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-17 22:22:21 +0530

    Time: 288 ms (41.35%), Space: 109.3 MB (49.36%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -25,15 +25,15 @@ public:
                     int y=j+dir[1];
                     if(x>=0&&y>=0&&x<m&&y<n&&visited[x][y]==false){
                         isWater[x][y]=isWater[i][j]+1;
-                         for(auto &dir: directions){
-                            int xx=x+dir[0];
-                            int yy=y+dir[1];
-                            if(xx>=0&&yy>=0&&xx<m&&yy<n&&visited[xx][yy]==true){
-                                if(abs(isWater[x][y]-isWater[xx][yy])>1){
-                                    isWater[x][y]=isWater[i][j];
-                                }
-                            }
-                       }
+                    //      for(auto &dir: directions){
+                    //         int xx=x+dir[0];
+                    //         int yy=y+dir[1];
+                    //         if(xx>=0&&yy>=0&&xx<m&&yy<n&&visited[xx][yy]==true){
+                    //             if(abs(isWater[x][y]-isWater[xx][yy])>1){
+                    //                 isWater[x][y]=isWater[i][j];
+                    //             }
+                    //         }
+                    //    }
                         visited[x][y]=true;
                         q.push({x,y});
                }
