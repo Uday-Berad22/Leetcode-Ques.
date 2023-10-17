45a9625568e381310d759f339cdefb23f7407ff5
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-17 15:19:16 +0530

    Time: 11 ms (12.67%), Space: 13.6 MB (32.06%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -10,42 +10,62 @@ class Myclass{
     }
 };
 class Solution {
-    vector<vector<int>> directions={{1,0},{0,1},{0,-1},{-1,0}};
+    // vector<vector<int>> directions={{1,0},{0,1},{0,-1},{-1,0}};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        queue<Myclass> q;
-        int freshcnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    Myclass obj(i,j,0);
-                    q.push(obj);
-                    grid[i][j]=-1;
-                }
-                if(grid[i][j]==1)freshcnt++;
-            }
+        int rows = grid.size();
+        if (rows == 0) {
+            return -1;
         }
-        int ans=0;
-        int count=0;
-        int cnt=q.size();
-        while(!q.empty()){
-            Myclass obj=q.front();
-            q.pop();
-            count++;
-            ans=max(ans,obj.time);
-            for(auto &dir: directions){
-                int i=obj.x+dir[0];
-                int j=obj.y+dir[1];
-                if(i>=0&&i<m&&j>=0&&j<n&&grid[i][j]==1&&grid[i][j]!=-1){
-                    Myclass object(i,j,obj.time+1);
-                    q.push(object);
-                    grid[i][j]=-1;
+        int cols = grid[0].size();
+        
+        int fresh_cnt = 0;
+        queue<pair<int, int>> rotten;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 2) {
+                    rotten.push(make_pair(r, c));
+                }
+                else if (grid[r][c] == 1) {
+                    fresh_cnt++;
                 }
             }
         }
-        if(count-cnt<freshcnt) return -1;
-        return ans;
+        
+        int minutes_passed = 0;
+        
+        while (!rotten.empty() && fresh_cnt > 0) {
+            minutes_passed++;
+            int level_size = rotten.size();
+            
+            for (int i = 0; i < level_size; i++) {
+                int x = rotten.front().first;
+                int y = rotten.front().second;
+                rotten.pop();
+                
+                vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+                
+                for (const auto& dir : directions) {
+                    int xx = x + dir.first;
+                    int yy = y + dir.second;
+                    
+                    if (xx < 0 || xx == rows || yy < 0 || yy == cols) {
+                        continue;
+                    }
+                    
+                    if (grid[xx][yy] == 0 || grid[xx][yy] == 2) {
+                        continue;
+                    }
+                    
+                    fresh_cnt--;
+                    grid[xx][yy] = 2;
+                    rotten.push(make_pair(xx, yy));
+                }
+            }
+        }
+        
+        return (fresh_cnt == 0) ? minutes_passed : -1;
     }
+
 };
