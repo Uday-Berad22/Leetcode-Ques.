b32ff7a49eb70aa9cd0c2f3e8065ee00d2b2e6d6
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-17 13:37:17 +0530

    Time: 3 ms (88.09%), Space: 13.9 MB (10.86%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,52 @@
+class Myclass{
+    public:
+    int x;
+    int y;
+    int time;
+    Myclass(int x,int y,int time){
+        this->x=x;
+        this->y=y;
+        this->time=time;
+    }
+};
+class Solution {
+    vector<vector<int>> directions={{1,0},{0,1},{0,-1},{-1,0}};
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        queue<Myclass> q;
+        int freshcnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    Myclass obj(i,j,0);
+                    q.push(obj);
+                }
+                if(grid[i][j]==1)freshcnt++;
+            }
+        }
+        int ans=0;
+        int count=0;
+        int cnt=q.size();
+        while(!q.empty()){
+            Myclass obj=q.front();
+            q.pop();
+            if(visited[obj.x][obj.y]==true) continue;
+            visited[obj.x][obj.y]=true;
+            count++;
+            ans=max(ans,obj.time);
+            for(auto &dir: directions){
+                int i=obj.x+dir[0];
+                int j=obj.y+dir[1];
+                if(i>=0&&i<m&&j>=0&&j<n&&grid[i][j]==1&&visited[i][j]==false){
+                    Myclass object(i,j,obj.time+1);
+                    q.push(object);
+                }
+            }
+        }
+        if(count-cnt<freshcnt) return -1;
+        return ans;
+    }
+};
