018954b2d382f4a8f419e1aaf4905e3c369b2b82
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-18 09:52:46 +0530

    Time: 352 ms (47.66%), Space: 143.6 MB (46.94%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
         vector<vector<int>> graph(n+1);
-        unordered_map<int,int> m;
-        for(int i=1;i<n+1;i++) m[i]+=0;
+        unordered_map<int,int> indegree;
+        for(int i=1;i<n+1;i++) indegree[i]=0;
         for(int i=0;i<relations.size();i++){
             graph[relations[i][0]].push_back(relations[i][1]);
-            m[relations[i][1]]++;
+            indegree[relations[i][1]]++;
         }
         queue<pair<int,int>> q;
         vector<bool> visited(n+1,false);
-        for(auto &a: m){
+        for(auto &a: indegree){
             if(a.second==0){
                 visited[a.first]=true;
                 q.push({a.first,time[a.first-1]});
@@ -21,16 +21,15 @@ public:
         while(!q.empty()){
             int x=0;
             int qsize=q.size();
-            // queue<pair<int,int>> temp;
             for(int i=0;i<qsize;i++){
                 pair<int,int> node=q.front();
                 q.pop();
                 ans=max(ans,node.second);
                 for(auto &nbr:graph[node.first]){
                     if(visited[nbr]==false){
-                        m[nbr]--;
+                        indegree[nbr]--;
                         ntime[nbr-1]=max(ntime[nbr-1],time[nbr-1]+node.second);
-                        if(m[nbr]==0){
+                        if(indegree[nbr]==0){
                             q.push({nbr,ntime[nbr-1]});
                             visited[nbr]=true;
                         }
