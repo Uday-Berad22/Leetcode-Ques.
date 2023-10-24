a2d175235371f717aee4b884640ad51a24e0da71
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-24 23:48:12 +0530

    Time: 61 ms (35.49%), Space: 27 MB (91.26%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,17 +1,29 @@
 class Solution {
      vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
   public:
-    void DFS(int x,int y,vector<vector<int>> &mat){
+    void BFS(int i,int j,vector<vector<int>> &mat){
         int n=mat.size();
         int m=mat[0].size();
-        mat[x][y]=0;
-        for(auto &dir: directions)
-        {
-            int xx=x+dir[0];
-            int yy=y+dir[1];
-            if(xx<n&&yy<m&&xx>=0&&yy>=0&&mat[xx][yy]==1)
-                DFS(xx,yy,mat);
-        }       
+        
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        mat[i][j]=0;
+        while(!q.empty()){
+            pair<int,int> p=q.front();
+            int x=p.first;
+            int y=p.second;
+            q.pop();
+            for(auto &dir: directions)
+            {
+                int xx=x+dir[0];
+                int yy=y+dir[1];
+                if(xx<n&&yy<m&&xx>=0&&yy>=0&&mat[xx][yy]==1)
+                {
+                    q.push({xx,yy});
+                    mat[xx][yy]=0;
+                }
+            } 
+        }      
     }
     int numEnclaves(vector<vector<int>> &mat) {
         // Code here
@@ -19,12 +31,16 @@ class Solution {
         int n=mat.size();
         int m=mat[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0||i==n-1||j==0||j==m-1){
-                    if(mat[i][j]==1)
-                    DFS(i,j,mat);
-                }
-            }
+            if(mat[i][0]==1)
+            BFS(i,0,mat);
+            if(mat[i][m-1]==1)
+            BFS(i,m-1,mat);
+        }
+        for(int i=0;i<m;i++){
+            if(mat[0][i]==1)
+            BFS(0,i,mat);
+            if(mat[n-1][i]==1)
+            BFS(n-1,i,mat);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
