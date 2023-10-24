f1bf70df6fcd10ae3180959faadfb006d41b639b
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-24 23:25:52 +0530

    Time: 19 ms (9.09%), Space: 23.9 MB (5.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,39 +1,47 @@
 class Solution {
 vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-    void DFS(int x,int y,vector<vector<char>> &mat,vector<vector<bool>> &visited,bool &boundary){
+    void DFS(int x,int y,vector<vector<char>> &mat,bool &boundary){
         int n=mat.size();
         int m=mat[0].size();
-        visited[x][y]=true;
         if(boundary==false){
             mat[x][y]='X';
+        }
+        else{
+            mat[x][y]='-';
         }
         for(auto &dir: directions)
         {
             int xx=x+dir[0];
             int yy=y+dir[1];
-            if(xx<n&&yy<m&&xx>=0&&yy>=0&&visited[xx][yy]==false&&mat[xx][yy]=='O')
-                DFS(xx,yy,mat,visited,boundary);
+            if(xx<n&&yy<m&&xx>=0&&yy>=0&&mat[xx][yy]=='O')
+                DFS(xx,yy,mat,boundary);
         }       
     }
     void solve(vector<vector<char>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
         bool boundary=true;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0||i==n-1||j==0||j==m-1){
-                    if(visited[i][j]==false&&mat[i][j]=='O')
-                    DFS(i,j,mat,visited,boundary);
+                    if(mat[i][j]=='O')
+                    DFS(i,j,mat,boundary);
                 }
             }
         }
         boundary=false;
         for(int i=1;i<n-1;i++){
             for(int j=1;j<m-1;j++){
-                    if(visited[i][j]==false&&mat[i][j]=='O')
-                    DFS(i,j,mat,visited,boundary);
+                    if(mat[i][j]=='O')
+                    DFS(i,j,mat,boundary);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]=='-'){
+                    mat[i][j]='O';
+                }
             }
         }
     }
