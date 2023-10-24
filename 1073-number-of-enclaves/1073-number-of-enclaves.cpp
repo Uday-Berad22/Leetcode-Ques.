16b6b7fd914a4c35bbcad098e29f258d234d8b5e
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-24 23:41:28 +0530

    Time: 57 ms (56.4%), Space: 42.8 MB (13.16%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,38 @@
+class Solution {
+     vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
+  public:
+    void DFS(int x,int y,vector<vector<int>> &mat){
+        int n=mat.size();
+        int m=mat[0].size();
+        mat[x][y]=0;
+        for(auto &dir: directions)
+        {
+            int xx=x+dir[0];
+            int yy=y+dir[1];
+            if(xx<n&&yy<m&&xx>=0&&yy>=0&&mat[xx][yy]==1)
+                DFS(xx,yy,mat);
+        }       
+    }
+    int numEnclaves(vector<vector<int>> &mat) {
+        // Code here
+        int count=0;
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||i==n-1||j==0||j==m-1){
+                    if(mat[i][j]==1)
+                    DFS(i,j,mat);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                   count++;
+                }
+            }
+        }
+        return count;
+    }
+};
