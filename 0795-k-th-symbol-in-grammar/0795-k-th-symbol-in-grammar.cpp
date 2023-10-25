93a4f21c8ab366c7fbc0d89235034f9887549b3e
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-25 10:54:59 +0530

    Time: 2 ms (51.37%), Space: 6.3 MB (53.37%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1){
-            return 0;
-        }
-        if(k%2==0){
-            if(kthGrammar(n-1,k/2)==0){
-                return 1;
-            }
-            return 0;
-        }
-        else{
-            if(kthGrammar(n-1,(k+1)/2)==0){
-                return 0;
-            }
-            return 1;
-        }
+       return __builtin_popcount(k-1)%2==0?0:1;
     }
 };
